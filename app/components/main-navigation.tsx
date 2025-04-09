958d527799f1ff2e7d175a827d91ce79ca6111a7
Amir3629 <amirhossein.firoozi@hotmail.com>
2025-04-09 22:40:12 +0200

    Fix main-navigation component

--- app/components/main-navigation.tsx
+++ app/components/main-navigation.tsx
@@ -1,6 +1,27 @@
-<Link
-  href={link.href}
-  className="text-white hover:text-white/80 transition-colors duration-300"
->
-  {link.label}
-</Link> 
+"use client"
+
+import React from 'react'
+import Link from 'next/link'
+
+interface NavigationLinkProps {
+  href: string
+  className?: string
+  children: React.ReactNode
+}
+
+export const NavigationLink: React.FC<NavigationLinkProps> = ({
+  href,
+  className = "text-white hover:text-white/80 transition-colors duration-300",
+  children
+}) => {
+  return (
+    <Link
+      href={href}
+      className={className}
+    >
+      {children}
+    </Link>
+  )
+}
+
+export default NavigationLink 
